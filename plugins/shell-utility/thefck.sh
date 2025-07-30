cc0cc28a7cb520b28f9d97ac15675a95c20ffc5c
snasir-dev <103860247+snasir-dev@users.noreply.github.com>
2025-07-29 20:11:15 -0400

    feat(thef***): Add script which define aliases ('fix', 'oops', 'why') for thef*** shell utility plugin.

---
+++ plugins/shell-utility/thefck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# --- ENVIRONMENT VARIABLES ---
+
+# --- FUNCTIONS ---
+
+# --- ALIASES ---
+# shellcheck disable=SC2046
+eval $(thefuck --alias fix)
+eval $(thefuck --alias oops)
+eval $(thefuck --alias why)
+
+# --- COMPLETIONS ---
