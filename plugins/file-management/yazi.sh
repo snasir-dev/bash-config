4f0be682ac0c32791eadbcd1cebfed3a18b21d35
snasir-dev <103860247+snasir-dev@users.noreply.github.com>
2025-09-29 16:06:40 -0400

    feat(yazi.sh): add configuration setup with environment variable and initialization function
    refractor(yazi): Update function to better create yazi config directories and properly set yazi config home env variable
    
    refractor(yazi.sh): Add comments clarifying where to see themes repo in yazi.sh

--- plugins/file-management/yazi.sh
+++ plugins/file-management/yazi.sh
@@ -6,7 +6,16 @@
 # Docs: https://yazi-rs.github.io/docs/quick-start
 # ============================================
 
-# --- ENVIRONMENT VARIABLES ---
+# --- ENVIRONMENT VARIABLES / PATH ---
+# By default Yazi uses '%AppData%\yazi\config\yazi.toml' on Windows
+# To set ~/.config/yazi, must set that value in XDG_CONFIG_HOME env variable.
+# Docs: https://yazi-rs.github.io/docs/configuration/overview#custom-directory
+export YAZI_CONFIG_HOME="$HOME/.config/yazi"
+
+# --- CONFIGURATION ---
+# Docs: https://yazi-rs.github.io/docs/configuration/overview
+# Yazi has default files (yazi-default, keymap-default, theme-dark.toml | theme-light.toml)
+# To override, place a (yazi.toml, keymap.toml, theme.toml) file in the config directory. (For us will be $HOME/.config)
 
 # --- FUNCTIONS ---
 # 'y' shell wrapper that provides the ability to change the current working directory when exiting Yazi.
@@ -19,8 +28,33 @@ function y() {
     rm -f -- "$tmp"
 }
 
+# Function to bootstrap Yazi config directory and empty files
+yazi_create_config() {
+    local config_dir="$HOME/.config/yazi"
+
+    # Create root directory as well as "flavors" directory which is for themes.
+    mkdir -p "$config_dir" "$config_dir/flavors"
+
+    # Create empty config files if missing
+    touch "$config_dir/yazi.toml" "$config_dir/keymap.toml" "$config_dir/theme.toml"
+
+    # ya (DO NOT NEED TO SEPARATELY INSTALL )- built-in package manager comes bundled with YAZI dedicated to handling Yazi's themes and plugins
+    # Install Theme - Ayu Dark (Themes gets installed in the flavors directory.)
+    # List of all themes - REPO: https://github.com/yazi-rs/flavors/blob/main/README.md
+    ya pkg add kmlupreti/ayu-dark
+    # ya pkg add gosxrgxx/flexoki-dark
+    ya pkg add 956MB/vscode-dark-modern
+    # ya pkg add 956MB/vscode-dark-plus
+    # ya pkg add bennyyip/gruvbox-dark
+
+    echo -e "📃Listing pkg packages:\n"
+    ya pkg list
+
+    echo -e "\n✅ Yazi config initialized: $config_dir/yazi.toml, $config_dir/keymap.toml, $config_dir/theme.toml"
+}
+
 # --- ALIASES ---
-# This will trigger y() instead of the yazi.exe executable. (If for any reason you need to trigger yazi.exe, run command 'command yazi')
+# This will trigger y() function above instead of the yazi.exe executable. (If for any reason you need to trigger yazi.exe, run command 'command yazi')
 alias yazi='y'
 
 # --- COMPLETIONS ---
