5cf7abdecdfaeaed1cb5c5e55af217e41344f2bc
snasir-dev <103860247+snasir-dev@users.noreply.github.com>
2025-10-09 00:14:55 -0400

    feat(ripgrep.sh): Add ripgrep.sh information on how it works and how to use it

--- plugins/text/pattern-matching/ripgrep.sh
+++ plugins/text/pattern-matching/ripgrep.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-# TODO - IMPLEMENT THIS
+# ripgrep (rg): Much faster and better alternative to grep. ripgrep recursively searches your current directory for a regex pattern while respecting your .gitignore.
+
+# RESOURCES:
+# Github: https://github.com/BurntSushi/ripgrep
+# Interactive Ripgrep - TUTORIAL/GUIDE (OFFICIAL SOURCE): https://codapi.org/try/ripgrep/
+# Playground ripgrep - Has Typical Use Cases (Official Source): https://codapi.org/ripgrep/
+
+# --- ENVIRONMENT VARIABLES ---
+
+# --- FUNCTIONS ---
+
+# --- ALIASES ---
+
+# --- COMPLETIONS ---
